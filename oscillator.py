7862e0728dd693bc435a6e30b277a1ac100db020
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2020-08-20 13:07:32 -0500

    tidy up per alex

--- oscillator.py
+++ oscillator.py
@@ -4,20 +4,21 @@
 Ball on a Spring
 Charles Fyfe
 Fall 2020
+
+Numerical values are arbitrary. Don't be shy about modifying initial size,
+velocity, spring constant, etc.
 """
 
 # You may be used to "from vpython import *"
 # Best practice is to instead import the whole namespace. This makes debugging
 # a lot easier. What would happen, for example, if you imported two libraries
 # with two different definitions of the "vector" object?
-
-
 import vpython
 
 
 def main():
     graph = init_graph()
-    spring, ball = init_setup()
+    spring, ball = init_spring_and_ball()
     # Outsourcing setup to helper functions keeps our main function legible!
     t, dt, tmax = 0, 0.1, 30
     while t < tmax:
@@ -26,12 +27,13 @@ def main():
         vpython.rate(1/dt)
         # VPython vector objects have nice helper methods for when we need
         # magnitudes and unit vectors.
-        stretch = spring.axis.mag - spring.x0
+        length = spring.axis.mag
+        stretch = length - spring.length_relax
         direction = spring.axis.hat
         # Physics happens here.
-        force = -spring.k*stretch*direction
-        ball.v += force*dt/ball.m
-        ball.pos += ball.v*dt
+        force = -spring.spring_constant*stretch*direction
+        ball.velocity += force*dt/ball.mass
+        ball.pos += ball.velocity*dt
         # Update the spring to stay connected to the ball
         spring.axis = ball.pos - spring.pos
         # Add a point to the graph every iteration
@@ -49,11 +51,15 @@ def init_graph():
     return vpython.gcurve(color=vpython.color.red, width=2)
 
 
-def init_setup():
-    # No need to return the box object, since it doesn't change
+def init_spring_and_ball():
+    # Draw a box to anchor the spring, but don't bother naming or returning it.
+    # It does not change over time.
     box_size = 20
+    # Position is the center of the box. We want the edge at the origin.
+    box_center = vpython.vector(-0.5*box_size, 0, 0)
     vpython.box(
-        pos=vpython.vector(-0.5*box_size, 0, 0),
+        # Position is the center of the box. We want the edge at the origin.
+        pos=box_center,
         width=box_size,
         length=box_size,
         height=box_size,
@@ -63,13 +69,14 @@ def init_setup():
     # then add additional attributes for convenient bookkeeping.
     spring_length_relax = 10
     spring_length_start = 5
+    spring_axis = vpython.vector(spring_length_start, 0, 0)
     spring = vpython.helix(
         pos=vpython.vector(0, 0, 0),
         coils=10,
-        axis=vpython.vector(spring_length_start, 0, 0),
+        axis=spring_axis,
     )
-    spring.k = 1
-    spring.x0 = spring_length_relax
+    spring.spring_constant = 1
+    spring.length_relax = spring_length_relax
     # Same deal for the ball. Position, radius, and color are all we need to
     # draw the initial object. Additional values are used later.
     ball_radius = 1
@@ -78,10 +85,15 @@ def init_setup():
         radius=ball_radius,
         color=vpython.color.red,
     )
-    ball.m = 1
-    ball.v = vpython.vector(0, 0, 0)
+    ball.mass = 1
+    ball.velocity = vpython.vector(0, 0, 0)
     return spring, ball
 
 
+# This is a pretty typical setup for a Python script: we define a bunch of
+# functions up top, then call main at the bottom to start actually doing the
+# work. Notably, main only gets called if we run this script directly. If we
+# import it into another script (for example, to reuse the helper functions)
+# then this will not call main.
 if __name__ == "__main__":
     main()
